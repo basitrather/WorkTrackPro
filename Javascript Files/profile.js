80f56113ec16048511f12a7dc751d13a9e3be857
Basit <ratherbasit253@gmail.com>
2026-01-27 06:05:30 +0530

    UI redesign and userCount updated on page refresh

--- Javascript Files/profile.js
+++ Javascript Files/profile.js
@@ -1,26 +1,43 @@
 import { supabase } from "./supabase";
+
+//IFIE functions
 (async function () {
   const {
     data: { user },
   } = await supabase.auth.getUser();
-  console.log(user);
   if (!user) {
     window.location.href = "index.html";
   }
 })();
-//Selectors
-const userName = document.getElementById("userNameInput");
-const userEmail = document.getElementById("userEmailInput");
-const changePasswordLink = document.getElementById("changePasswordLink");
-const logOutBtn = document.querySelector(".logout-btn");
-
 (async function () {
   const {
     data: { user },
   } = await supabase.auth.getUser();
   userName.value = user.identities[0].identity_data.display_name;
+  navUserName.textContent = user.identities[0].identity_data.display_name;
   userEmail.value = user.email;
 })();
+(async function () {
+  let { data: user_roles, error } = await supabase
+    .from("user_roles")
+    .select("*");
+  totaluserCount.textContent = user_roles.length;
+})();
+
+//Selectors
+const userName = document.getElementById("userNameInput");
+const userEmail = document.getElementById("userEmailInput");
+const changePasswordLink = document.getElementById("changePasswordLink");
+const logOutBtn = document.querySelector(".logout-btn");
+const navUserName = document.querySelector(".nav-user-name");
+const ProfileSection = document.querySelector(".profile-section");
+const sideBar = document.querySelector(".sidebar-menu");
+const topBar = document.querySelector(".stats");
+const sideBarOpts = document.querySelectorAll("ul li");
+const totaluserCount = document.querySelector(".totalusers");
+
+//Show content Function
+const showContent = function (event) {};
 
 //Logout Function
 const logOut = async function () {
@@ -34,11 +51,24 @@ const resetPassword = async function () {
     userEmail.value,
   );
   window.location.href = "resetProfile.html";
-  console.log(data);
 };
+
 changePasswordLink.addEventListener("click", function () {
   resetPassword();
 });
 logOutBtn.addEventListener("click", function () {
   logOut();
+});
+
+sideBar.addEventListener("click", function (e) {
+  const btnClicked = e.target.closest("li");
+
+  if (!btnClicked) return;
+  sideBarOpts.forEach((li) => {
+    if (li !== btnClicked) {
+      li.classList.remove("active");
+    }
+  });
+  btnClicked.classList.add("active");
+  showContent(btnClicked);
 });
